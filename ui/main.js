4b4580e540c93425ddf0047b1832594fd75684d5
DeepakSridhar <deep1994rockz@gmail.com>
2016-09-29 05:18:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,14 @@ var marginLeft=0;
 function moveleft(){
     marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+'px';
+    if (marginLeft>500)
+    marginLeft=marginLeft-5;
+    img.style.marginLeft=marginLeft+'px';
 }
 
 img.onclick=function(){
     img.style.marginRight='100px';
     var interval= setInterval(moveleft,50);
+    if(marginLeft==50)
+     img.style.marginRight='0px';
 }
