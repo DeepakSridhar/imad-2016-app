68432d1f5c4ce3e5ddd6d3a716d64e90c026f865
DeepakSridhar <deep1994rockz@gmail.com>
2016-09-29 09:40:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,8 @@ button.onclick= function(){
     
     var request=new XMLHttpRequest();
     request.onreadystatechange= function(){
-        if(request.readyState==XMLHttpRequest.DONE){
-            if(request.status==200)
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200)
             var counter=request.responseText;
             var span= document.getElementById('count')
     span.innerHTML= counter.toString();
@@ -41,8 +41,8 @@ submit.onclick=function(){
     
     var request=new XMLHttpRequest();
     request.onreadystatechange= function(){
-        if(request.readyState==XMLHttpRequest.DONE){
-            if(request.status==200)
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200)
             {
     var names=request.responseText;
 names= JSON.parse(names);
