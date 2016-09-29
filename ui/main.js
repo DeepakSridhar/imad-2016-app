3eca505e270d516c4321e36bae229d0249ff90d6
DeepakSridhar <deep1994rockz@gmail.com>
2016-09-29 05:57:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,12 +16,19 @@ img.onclick=function(){
     if(marginLeft==505)
      img.style.marginRight='0px';
 }
-var counter=0;
+
 var button=document.getElementById('counter');
 button.onclick= function(){
     
-    
-    counter=counter+1;
-    var span= document.getElementById('count')
+    var request=new XMLHttpRequest();
+    request.onreadystatechange= function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200)
+            var counter=request.responseText;
+            var span= document.getElementById('count')
     span.innerHTML= counter.toString();
+        }
+    }
+    
+    
 }
