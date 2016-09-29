14c5796b8bde14c4a2589c49475260c722fb5acf
DeepakSridhar <deep1994rockz@gmail.com>
2016-09-29 08:34:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,14 +60,36 @@ var list='';
     var name=nameInput.value;
     request.open('GET','http://deepaksridhar.imad.hasura-app.io/submit-name?name='+ name,true);
     request.send(null);
-    
-    
-    
-    
 };
 
 
+var submit1=document.getElementById('submitbtn1');
+submit1.onclick=function(){
+    
+    var request=new XMLHttpRequest();
+    request.onreadystatechange= function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200)
+            {
+    var names=request.responseText;
+names= JSON.parse(names);
+var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+        
+        }
+        var ul= document.getElementById('namelist1');
+        ul.innerHTML=list;
+        }
+    }
+};
+    var nameInput=document.getElementById('comment');
+    var name=nameInput.value;
+    request.open('GET','http://deepaksridhar.imad.hasura-app.io/submit-name?name='+ name,true);
+    request.send(null);
+    
 
+};
 
 
 
